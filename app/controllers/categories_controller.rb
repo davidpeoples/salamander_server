03625e1cae791815133eee9f3133a9204143f540
David Peoples <david.a.peoples@gmail.com>
2011-01-26 01:09:26 -0600

    Selector for parent category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,7 @@ class CategoriesController < ApplicationController
   # GET /categories
   # GET /categories.xml
   def index
-    @categories = Category.all
+    @categories = Category.all(:order => "lft")
 
     respond_to do |format|
       format.html # index.html.erb
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,35 @@
 class Category < ActiveRecord::Base
 	acts_as_nested_set
+
+	def full_name
+		category_tree = []
+		self.self_and_ancestors.each { |v| category_tree << v.name }
+		category_tree.join(", ")
+	end
+
+	def valid_parents
+		if self.id.nil? then
+			Category.all(:order => "lft")
+		else
+			valid_parents = []
+			Category.all(:order => "lft").each { |v|
+				valid_parents << v if ! v.is_or_is_descendant_of?(self)
+			}
+			valid_parents
+		end
+	end
+	
+	def Category.valid_parents(id)
+		if id.nil? then
+			Category.all(:order => "lft")
+		else
+			existing_category = Category.find(id)
+			valid_parents = []
+			Category.all(:order => "lft").each { |v|
+				valid_parents << v if ! v.is_or_is_descendant_of?(existing_category)
+			}
+			valid_parents
+		end
+	end
+
 end

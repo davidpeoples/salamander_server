80880b551623b2720ad19780f7cac9ac45468b30
David Peoples <david.a.peoples@gmail.com>
2011-01-25 20:39:59 -0600

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SalamanderServer::Application.config.secret_token = 'b8418f26e5648ca4840b5219a2d3b7f3383a89ffa5590f857129fa6ba328c628bf905f210f808c41fdb362ec99418a5b86fbab076dc17c35b9b7925799f85966'

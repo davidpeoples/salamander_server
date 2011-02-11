46a0bb233c730469205176233cc1e12550231b96
David Peoples <david.a.peoples@gmail.com>
2011-02-11 00:08:58 -0600

    About to return to master branch

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,12 @@
 class ProductsController < ApplicationController
   def index
 		@title = "Products"
+    @products = Product.all
   end
 
   def show
+    @title = "Product"
+    @product = Product.find(params[:id])
   end
 
   def new
